8f7261fa9a2548c84f8c8a94799f7d936fea6638
gquesnot <gaqu1994@gmail.com>
2021-11-24 17:45:07 +0100

    done

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const puppeteer = require("puppeteer");
 
-var result = []
 
+var result = []
 async function getTwitchChatUsersByStream(streamStr){
     const browser = await puppeteer.launch({
         headless: true,
     });
+
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height:1080})
-    page.on('response', response => {
+    page.on('response', async response => {
 
-        if (response.url() === "https://gql.twitch.tv/gql" && response.request().initiator().type !== "preflight"){
-            response.text().then(function (text){
-                if (text.includes("moderators")){
-                    result =  JSON.parse(text)
+        if (response.url() === "https://gql.twitch.tv/gql" && response.request().initiator().type !== "preflight") {
+            let text = await response.text()
+            if (text.includes("moderators")){
+                let body = JSON.parse(text)
+                for (let user of body[0].data.channel.chatters.viewers){
+                    result.push(user.login)
                 }
-            })
+            }
         }
-        // response.text().then(function (textBody) {
-        //     if (textBody.includes("moderators")){
-        //         console.log(textBody);
-        //     }
-        //
-        // })
     })
     await page.goto('https://www.twitch.tv/' + streamStr);
 
@@ -36,14 +33,14 @@ async function getTwitchChatUsersByStream(streamStr){
         //     Array.from(document.querySelectorAll("div.chat-viewers-list>div:nth-child(2) button")).map(d => d.getAttribute("data-username"))
         // )
     }
-    return []
-
 }
 
 var start = new Date().getTime();
-getTwitchChatUsersByStream("wardiii")
-    .then(r => {
-        console.log(result.data.channel.chatters.viewers ,result.data.channel.chatters.viewers.length)
+getTwitchChatUsersByStream("blastpremier")
+    .then(()=> {
+
+
+        console.log(result)
         var end = new Date().getTime();
         var time = end - start;
         console.log('Execution time: ' + time);
