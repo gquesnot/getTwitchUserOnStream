638d8de8be72fd34009cdb4b9adc5f46990c9da3
gquesnot <gaqu1994@gmail.com>
2021-11-24 13:20:49 +0100

    done

--- index.js
+++ index.js
@@ -5,26 +5,30 @@ function delay(time) {
     });
 }
 
-async function getTwitchAccountsByStream(streamStr){
+async function getTwitchChatUsersByStream(streamStr){
     const browser = await puppeteer.launch({
         headless: true,
     });
     const page = await browser.newPage();
+    await page.setViewport({width: 1920, height:1080})
     await page.goto('https://www.twitch.tv/' + streamStr);
 
 
     await page.waitForNavigation()
     let btn = await page.$("button[data-test-selector='chat-viewer-list']")
-    await btn.click();
-    await page.waitForSelector("div.chat-viewers-list>div:nth-child(2)");
-    const data = await page.evaluate(() =>
-        Array.from(document.querySelectorAll("div.chat-viewers-list>div:nth-child(2) button")).map(d => d.getAttribute("data-username"))
-    )
+    if (btn){
+        await btn.click();
+        await page.waitForSelector("div.chat-viewers-list>div:nth-child(2)");
+        return await page.evaluate(() =>
+            Array.from(document.querySelectorAll("div.chat-viewers-list>div:nth-child(2) button")).map(d => d.getAttribute("data-username"))
+        )
+    }
+    return []
 
-    console.log(data)
 
 
 }
 
 
-getTwitchAccountsByStream("ogamingsc2")
+getTwitchChatUsersByStream("ogamingsc2")
+    .then(r => console.log(r, r.length))
